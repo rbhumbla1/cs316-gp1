729e3e38de9cbd810d455ce27717c70f0e533787
FumoSapian <144274473+FumoSapian@users.noreply.github.com>
2023-11-01 12:42:38 -0700

    ..

--- script.js
+++ script.js
@@ -5,17 +5,51 @@ document.addEventListener("DOMContentLoaded", function () {
     const departureDatePickerButton = document.getElementById("show-departure-datepicker");
     const returnDatePickerButton = document.getElementById("show-return-datepicker");
 
-    // Initialize departure date picker
-    $(departureDateInput).datepicker({
-        dateFormat: "mm/dd/yy",
-        showButtonPanel: true,
-    });
+// document.addEventListener("DOMContentLoaded", function () {
+//     const departureDateInput = document.getElementById("departureDateInput");
+//     const returnDateInput = document.getElementById("return-date");
+//     const passengerCountSelect = document.getElementById("passenger");
+//     const form = document.querySelector("form");
 
-    // Initialize return date picker
-    $(returnDateInput).datepicker({
-        dateFormat: "mm/dd/yy",
-        showButtonPanel: true,
-    });
+//     if (!departureDateInput || !returnDateInput || !passengerCountSelect) {
+//         console.error("Departure, return date input, or passenger select not found.");
+//         return;
+//     }
+
+//     // Initialize departure date picker with the correct date format
+//     $(departureDateInput).datepicker({
+//         dateFormat: "yy-mm-dd",
+//         showButtonPanel: true,
+//     });
+
+//     // Initialize return date picker with the correct date format
+//     $(returnDateInput).datepicker({
+//         dateFormat: "yy-mm-dd",
+//         showButtonPanel: true,
+//     });
+
+//     // Function to scroll to flight results section
+//     function scrollToFlightResults() {
+//         const flightResultsSection = document.getElementById("flight-results");
+//         flightResultsSection.scrollIntoView({ behavior: 'smooth' });
+//     }
+
+    var api_request= 'https://api.flightapi.io/roundtrip/6542a549a398868dacdb5960/SFO/SEA/2023-11-03/2023-11-07/8/0/0/Economy/USD';
+    fetch(api_request)
+        .then(function (response) {
+             console.log("response", response);
+             return response.json();
+        })
+        .then(function (data){
+            console.log("data", data);
+            for (let i = 0; i < 5; i++)
+            {
+                console.log(data.legs[i].airlineCodes);
+                console.log(data.legs[i].departureTime);
+                console.log(data.legs[i].arrivalTime);
+            }
+            
+        });
 
     departureDatePickerButton.addEventListener("click", function () {
         $(departureDateInput).datepicker("show");
